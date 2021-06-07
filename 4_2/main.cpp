f565365705b154a6f53046c2ecc1d3c71fcc3ae5
hun0905 <a0905121101@gmail.com>
2021-06-07 10:26:31 -0700

    01:26

---
+++ 4_2/main.cpp
@@ -0,0 +1,40 @@
+#include"mbed.h"
+#include "bbcar.h"
+//BufferedSerial pc(USBTX,USBRX); //tx,rx
+BufferedSerial uart(D1,D0); //tx,rx
+Ticker servo_ticker;
+PwmOut pin5(D5), pin6(D6);
+BBCar car(pin5, pin6, servo_ticker);
+int main(){
+   uart.set_baud(9600);
+   bool stop = false;
+   while(1){
+      if(uart.readable()){
+            char recv[1];
+            uart.read(recv, sizeof(recv));
+            if(recv[0] == 's')
+            {
+               //car.stop();
+               car.goStraight(100);
+               ThisThread::sleep_for(200ms);
+               car.stop();
+               
+            }
+            /*else if(recv[0] == 'l')
+            {
+               
+               car.turn(100,0.3);
+               ThisThread::sleep_for(200ms);
+               car.stop();
+            }
+            else if(recv[0] == 'r')
+            {
+               
+               car.turn(100,-0.3);
+               ThisThread::sleep_for(200ms);
+              / car.stop();
+            }*/
+      }
+      
+   }
+}
---
+++ Python_PRC/Python_RPC.py
@@ -0,0 +1,68 @@
+import time
+import serial
+import sys,tty,termios
+class _Getch:
+    def __call__(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+def get():
+    inkey = _Getch()
+    while(1):
+        k=inkey()
+        if k!='':break
+    if k=='\x1b':
+        k2 = inkey()
+        k3 = inkey()
+        if k3=='A':
+            print ("up")
+            s.write("/goStraight/run 100 \n".encode())
+        if k3=='B':
+            print ("down")
+            s.write("/goStraight/run -100 \n".encode())
+        if k3=='C':
+            print ("right")
+            s.write("/turn/run 100 -0.3 \n".encode())
+        if k3=='D':
+            print ("left")
+            s.write("/turn/run 100 0.3 \n".encode())
+        time.sleep(1)
+        s.write("/stop/run \n".encode())
+    elif k=='q':
+        print ("quit") 
+        return 0
+    elif k=='p':
+        print ("park")
+        d1 = input("d1: ")
+        d2 = input("d2: ")
+        dir = input("direction: ")
+
+        s.write(f"/park/run {d2} \n".encode())
+        time.sleep(1)
+        s.write("/stop/run \n".encode())
+        if dir != 'n':
+            if dir == 'w':
+                s.write("/turn/run 100 -0.3 \n".encode())
+            elif dir == 'e':
+                s.write("/turn/run 100 0.3 \n".encode())
+                
+            time.sleep(3)
+            s.write("/stop/run \n".encode())
+            s.write(f"/park/run {d1} \n".encode())
+            time.sleep(1)
+            s.write("/stop/run \n".encode())
+    else:
+        print ("not an arrow key!")
+    return 1
+
+if len(sys.argv) < 1:
+    print ("No poprt input")
+s = serial.Serial(sys.argv[1])
+while get():
+    i = 0
